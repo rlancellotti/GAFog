09c08a5206e80d5cb644e08deab2a09722621331
Riccardo Lancellotti <riccardo.lancellotti@unimore.it>
2021-10-24 17:06:25 +0200

    Delete run_flask.sh

--- run_flask.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export FLASK_APP=flask_app.py
-export FLASK_ENV=development
-flask run
-# submit jobs with:
-# curl -X POST -H 'Content-Type: application/json' -d @sample_input2.json http://localhost:8080/api/v1.0/ga
