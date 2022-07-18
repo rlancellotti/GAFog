4806a058c3450c5fff9916ce8f7f3c6666f4ea5c
Gattopandacorno <gazzotti.caterina@gmail.com>
2022-07-18 15:04:10 +0200

    Better use of the code already implemented by solution.py

--- Mbfd/solution_mbfd.py
+++ Mbfd/solution_mbfd.py
@@ -1,5 +1,7 @@
 from math import sqrt
 import sys
+import json
+import argparse
 
 sys.path.append('../FogProblem')
 from problem import Problem
@@ -9,161 +11,131 @@ class Solution_mbfd(Solution):
 
     k = 10 # Number multiplied by the sum of meanserv(Sm or Tc); SLA
 
-    def __init__(self, problem:Problem):
-        self.problem     = problem                  # The problem
-        self.nf          = problem.get_nfog()       # Number of fog nodes
-        self.nsrv        = problem.get_nservice()   # Number of microservices
-        self.fog         = [None] * self.nf
-        self.sort_fog(problem)                      # Creates a dictiorary of fog sorted by capacity
-        self.sort_ms(problem)                       # List of microservices sorted by capacity(Sm or meanserv)
+    # TODO: chiamare super() e poi le funzioni specifiche implementate 
+    def __init__(self, problem:Problem, individual=[]):
+        
+        self.problem        = problem
+        self.nf             = problem.get_nfog()
+        self.fognames       = problem.get_fog_list()
+        self.nsrv           = problem.get_nservice()
+        self.service        = problem.get_microservice_list()
+        self.serviceidx     = self.get_service_idx()
+        if not(individual):
+            self.mapping = [None] * self.nsrv
+        else:
+            self.mapping = individual
+
+        self.fog            = [{}] * self.nf
+        self.resptimes      = None
+        self.queueingtimes  = None
+        self.waitingtimes   = None
+        self.servicetimes   = None
+        self.deltatime      = None
+        self.extra_param    = {}
+
+        self.sort_fog(problem)                          # A name's list of the fog in decreasing order by capacity
+        self.fog_initialize()                           # Init for self.fog
+        self.sort_ms(problem)                           # List of microservices sorted by capacity(Sm or meanserv)
         self.compute_fog_status() 
-        #self.compute_status()
-        self.resptimes   = {}                       # TODO: used to save/update service resptimes
-        self.extra_param = {}
 
 
     # It sorts fog nodes by capacity and initialize the status params
     def sort_fog(self, problem:Problem):
-        self.fognames = dict(sorted(problem.fog.items(), key=lambda x: x[0][1],reverse=True))
-        for node in self.fognames:
-            self.fognames[node]["microservice"] = [] #[0 for _ in range(self.problem.get_nservice())] 
-            self.fognames[node]["lamf"]         = 0.0
-            self.fognames[node]["Sf"]           = 0.0
-            self.fognames[node]["stdf"]         = 0.0
-            self.fognames[node]["mu"]           = 0.0
-            self.fognames[node]["cv"]           = 0.0
-            self.fognames[node]["rho"]          = 0.0
-            self.fognames[node]["Rf"]           = 0.0
+        self.fognames = list(dict(sorted(problem.fog.items(), key=lambda x: x[0][1],reverse=True)).keys())
+        
+        
+    def fog_initialize(self):
+        for fidx in range(self.nf):
+            self.fog[fidx]= {
+                'name': self.fognames[fidx],
+                'capacity': self.problem.get_fog(self.fognames[fidx])['capacity'],
+                'microservices': [],
+                'twait': 0.0,
+                }
 
     # It sorts microservices by Sm and returns a dictionary
     def sort_ms(self, problem:Problem):
         self.service = dict(sorted(problem.microservice.items(), key=lambda x: x[0][1], reverse=True))
 
-    def __str__(self):
-        return (str(self.fognames))
+    def get_ms_from_chain(self, msname='MS1_1'):
+        for k, c in self.problem.servicechain.items():
+            if msname in c['services']:
+                return k
+        return None
 
-    # Return all the microservice
-    def get_service_list(self):
-        return self.service
 
     # 'returns' the solution with a list of microservices names on a certain node
     def compute_fog_status(self):
 
-        Sf      = 0.0  
-        stdf    = 0.0
-        lamf    = 0.0
-        Rc      = 0.0
-        Sla     = 0.0
-        prevfog = None
+        tserv       = 0.0  
+        std         = 0.0
+        lam_tot     = 0.0
+        Sla         = 0.0
+        twait       = 0.0
+        X_func      = None # Its the obj_func for the first iteration  
 
-        for s in self.problem.sensor:
-            self.problem.sensor[s]["resptime"]  = 0.0
+        for ms in self.service :
+            if not self.mapping[self.get_service_idx()[ms]] is None: 
+                continue
+            
+            sc = self.get_ms_from_chain(ms)
 
-        for ms in self.service.keys():
-            Sf   += self.problem.microservice[ms]['lambda']*self.problem.microservice[ms]['meanserv'] 
-            stdf += self.problem.microservice[ms]['lambda']*(self.problem.microservice[ms]['stddevserv']**2 + self.problem.microservice[ms]['meanserv']**2)
-            lamf += self.problem.microservice[ms]['lambda']
+            tserv += self.problem.microservice[ms]['lambda']*self.problem.microservice[ms]['meanserv'] 
+            std += self.problem.microservice[ms]['lambda']*(self.problem.microservice[ms]['stddevserv']**2 + self.problem.microservice[ms]['meanserv']**2)
+            lam_tot += self.problem.microservice[ms]['lambda']
             Sla  += self.problem.microservice[ms]['meanserv'] 
 
-            for node in self.fognames:
-    
-                if lamf!=0:
+            for fidx in range(self.nf):
+                f = self.problem.get_fog(self.fognames[fidx])
+                if lam_tot!=0:
 
-                    Sf   /= lamf 
-                    Sf   /= self.fognames[node]['capacity']
-                    stdf  = sqrt((stdf/lamf)-(Sf**2)) 
-                    stdf /= self.fognames[node]['capacity']
+                    tserv /= lam_tot
+                    tserv /= f['capacity']
+                    std  = sqrt((std/lam_tot)-(tserv**2)) 
+                    std /= f['capacity']
 
-                    mu  = 1.0/Sf
-                    cv  = stdf/Sf
-                    rho = lamf/mu
-                    Rf  = super().mg1_waittime(lamf, mu, cv)
+                    mu  = 1.0/tserv
+                    cv  = std/tserv
+                    rho = lam_tot/mu
+                    twait  = super().mg1_waittime(lam_tot, mu, cv)
 
                 else:
-                    Sf = stdf = mu = cv = rho = Rf = 0
+                    tserv = std = mu = cv = rho = twait = 0
 
-                if ms not in [m for f in self.fognames.values() for m in f["microservice"]] \
-                    and lamf<mu and Rc<(self.k*Sla): 
-                 
-                    for s in self.problem.sensor:
-                        Rc += Rf
-                        if prevfog is not None: 
-                            Rc += self.problem.get_delay(prevfog, node)['delay']
-                            self.problem.sensor[s]["resptime"] += Rc
+                self.resptimes = super().compute_performance() # To compute resptime (Rc)
+                if ms not in [m for f in self.fog for m in f["microservices"]] \
+                    and lam_tot<mu and self.resptimes[sc]['resptime']<(self.k*Sla) \
+                    and (X_func==None or X_func>self.obj_func()):
 
-                        prevfog = node
                     
-                    self.fognames[node]["lamf"] = lamf
-                    self.fognames[node]["Sf"]   = Sf
-                    self.fognames[node]["stdf"] = stdf
-                    self.fognames[node]["mu"]   = mu
-                    self.fognames[node]["cv"]   = cv
-                    self.fognames[node]["rho"]  = rho
-                    self.fognames[node]["Rf"]   = Rf
 
-                    self.fognames[node]["microservice"].append(ms)
-    
-        print(self.fognames)  
-        print(self.problem.sensor)
+                    self.fog[fidx].update({
+                        'tserv': tserv, 
+                        'stddev': std, 
+                        'mu': mu, 
+                        'cv': cv,
+                        'lambda': lam_tot,
+                        'twait': twait,
+                        'tresp': twait + tserv,
+                        'rho': rho,
+                        }) 
+
+                    self.fog[fidx]['microservices'].append(ms)
+                    self.mapping[self.get_service_idx()[ms]] = fidx
+
+        print(self.fog)
+        print(self.resptimes)
+        print(self.mapping)
         
 
-    # 'returns' the solution with a list of 0/1 on a certain node
-    # 1 is used when the microservice is 'taken', 0 when is not
-    def compute_status(self):
-        i       = 0
-        Sf      = 0.0
-        stdf    = 0.0
-        lamf    = 0.0
-        Rc      = 0.0
-        Sla     = 0.0
-        prevfog = None
 
-        for s in self.problem.sensor:
-            self.problem.sensor[s]["resptime"] = 0.0
 
-        for ms in self.service.keys():
-            Sf   += self.problem.microservice[ms]['lambda']*self.problem.microservice[ms]['meanserv'] 
-            stdf += self.problem.microservice[ms]['lambda']*(self.problem.microservice[ms]['stddevserv']**2 + self.problem.microservice[ms]['meanserv']**2)
-            lamf += self.problem.microservice[ms]['lambda']
-            Sla  += self.problem.microservice[ms]['meanserv'] 
-
-            for node in self.fognames:
-
-                if lamf!=0:
-                    Sf   /= lamf 
-                    Sf   /= self.fognames[node]['capacity']
-                    stdf  = sqrt((stdf/lamf)-(Sf**2)) 
-                    stdf /= self.fognames[node]['capacity']
-
-                    mu  = 1.0/Sf
-                    cv  = stdf/Sf
-                    rho = lamf/mu
-                    Rf  = super().mg1_waittime(lamf, mu, cv)
-
-                else:
-                    Sf = stdf = mu = cv = rho = Rf = 0
-
-                if 1 not in [f["microservice"][i] for f in self.fognames.values()] \
-                    and lamf<mu and Rc<(self.k*Sla): 
-                 
-                    for s in self.problem.sensor:
-                        Rc += Rf
-                        if prevfog is not None: 
-                            Rc += self.problem.get_delay(prevfog, node)['delay']
-                            self.problem.sensor[s]["resptime"] += Rc
-
-                        prevfog = node
-                    
-                    self.fognames[node]["lamf"] = lamf
-                    self.fognames[node]["Sf"]   = Sf
-                    self.fognames[node]["stdf"] = stdf
-                    self.fognames[node]["mu"]   = mu
-                    self.fognames[node]["cv"]   = cv
-                    self.fognames[node]["rho"]  = rho
-                    self.fognames[node]["Rf"]   = Rf
-
-                    self.fognames[node]["microservice"][i] = 1
-            i += 1
-
-        #print(self.problem.sensor)
-        print(self.fognames)  
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', help='input file. Default sample_input2.json')
+    args = parser.parse_args()
+    fname = args.file if args.file is not None else 'sample_input2.json'
+    with open(fname,) as f:
+        data = json.load(f)
+    problem  = Problem(data)
+    solution = Solution_mbfd(problem, [0, None, None]) 
