733d1fc4603cd89695dd95b3bd190e4e1a0b1b8f
Gattopandacorno <gazzotti.caterina@gmail.com>
2022-07-26 16:06:36 +0200

    setup poetry

---
+++ gafog/Mbfd/__main__.py
@@ -0,0 +1,30 @@
+import os
+import json
+import argparse
+import time
+from datetime import timedelta
+
+from ..fog_problem.problem import Problem
+from ..fog_problem.solution import Solution
+from .solution_mbfd import SolutionMbfd
+from .mbfd import solve_problem
+
+
+st = time.monotonic()  # Start time, when it begins the program
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="input file. Default sample_input2.json")
+args   = parser.parse_args()
+fname  = args.file or "sample_input2.json"
+
+with open(fname) as f:
+    data = json.load(f)
+
+sol = solve_problem(data)
+
+with open("new_output.json", "w") as f:
+    json.dump(sol.dump_solution(), f, indent=2)
+
+
+et = time.monotonic()  # end time, when its finished the execution of the program
+print(f"execution time: {timedelta(et - st)}")  # TODO: should i change it?
--- gafog/Mbfd/mbfd.py
+++ gafog/Mbfd/mbfd.py
@@ -1,16 +1,10 @@
-import os
-import json
-import argparse
-import time
-from datetime import timedelta
-
 from ..fog_problem.problem import Problem
 from ..fog_problem.solution import Solution
 from .solution_mbfd import SolutionMbfd
 
 
 # Data is a json file
-def solve_problem(data:json):
+def solve_problem(data:dict):
     problem = Problem(data)
 
     return  SolutionMbfd(problem) 
@@ -22,27 +16,3 @@ def compare(solMBFD:SolutionMbfd):
         return True
     
     return False
-
-
-
-# Takes a file .json as the instance of the problem and it calls the solver 
-if __name__ == "__main__":
-    st  = time.monotonic() # Start time, when it begins the program
-
-    
-    parser   = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', help='input file. Default sample_input2.json')
-    args     = parser.parse_args()
-    fname    = args.file if args.file is not None else 'sample_input2.json'
-    
-    with open(fname,) as f:
-        data = json.load(f)
-    
-    sol = solve_problem(data)
-    
-    with open('new_output.json', 'w') as f:
-        json.dump(sol.dump_solution(), f, indent=2)
-
-    
-    et  = time.monotonic() # end time, when its finished the execution of the program
-    print(f'execution time: {timedelta(et - st)}') # TODO: should i change it?
