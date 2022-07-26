74d35c1e513881075ff603d23894b5b043739a41
Gattopandacorno <gazzotti.caterina@gmail.com>
2022-07-26 16:21:04 +0200

    move samples in a unique directory

--- gafog/Mbfd/__main__.py
+++ gafog/Mbfd/__main__.py
@@ -15,16 +15,16 @@ st = time.monotonic()  # Start time, when it begins the program
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="input file. Default sample_input2.json")
 args   = parser.parse_args()
-fname  = args.file or "sample_input2.json"
+fname  = args.file or "sample/sample_input2.json"
 
 with open(fname) as f:
     data = json.load(f)
 
 sol = solve_problem(data)
 
-with open("new_output.json", "w") as f:
+with open("sample/new_output.json", "w") as f:
     json.dump(sol.dump_solution(), f, indent=2)
 
 
 et = time.monotonic()  # end time, when its finished the execution of the program
-print(f"execution time: {timedelta(et - st)}")  # TODO: should i change it?
+print(f"execution time: {timedelta(et - st)}")  
